21f5b04557653d6c4b1c1cba7a48e7f0ec3deacd
Mencrypto <ricardo.villalobos@nube.sep.gob.mx>
2021-07-22 21:30:00 -0500

    Add variables to main.tf and set this variables in another file

--- main.tf
+++ main.tf
@@ -2,19 +2,28 @@ provider "aws" {
     region = "us-east-1"
 }
 
+variable "ami_id" {
+    type= string
+    default = "ami-0dc2d3e4c0f9ebd18"
+}
+
+variable "instance_type" {
+    type= string
+}
+
+variable "tags" {
+    type= map
+}
+
 resource "aws_instance" "testIntance" {
     #The ami is a last free tier and can be obtanied when you launch a new VM
-    ami = "ami-0dc2d3e4c0f9ebd18"
+    ami = var.ami_id
     associate_public_ip_address = true
-    instance_type = "t2.micro"
+    instance_type = var.instance_type
     ##The Key Should be generate previously in the aws account
     key_name = "terraform_key"
     security_groups = ["terraform_SSH_HTTP"]
-    tags = {
-        Name = "Public01",
-        Enviroment = "DEV"
-        terraform = "terraform"
-    }  
+    tags = var.tags
 }
 
 #Show the public IP to connect to the instance
